051c3f63434d47e18e1c17a51c0053091a2ae236
Vu Pham <vupham0326@mail.com>
2015-05-21 03:38:11 +0000

    building a model test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
+	
+	test "unique_tag" do 
+		quote = Quote.create(:author => "Vu Pham", :saying => "You only yolo once.")
+		expected = "VP#" + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual 
+	end
+
 end
