ec05ff896f56826f5a2c557f10d90d583d604371
Vu Pham <vupham0326@mail.com>
2015-05-21 09:16:22 +0000

    building a controller test

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -16,6 +16,13 @@ class QuotesController < ApplicationController
 	
 	end
 
+	def show
+		@quote = Quote.where(:id => params[:id]).first
+		if @quote.blank?
+			render :text => "Not Found", :status => :not_found
+		end
+
+	end
 
 	private
 
